dfdc38072ba970d8373cf9943c9a3fc6a2c746ba
Kobi Jones <kobijonesmail@gmail.com>
2023-09-17 23:26:18 -0500

    Final maximum subarray sum solution in Haskell

---
+++ haskell/MaximumSubarraySum.hs
@@ -0,0 +1,14 @@
+import Control.Monad (join, void)
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Foldable (Foldable (foldl'))
+
+-- My final solution
+maxSequence :: [Int] -> Int
+maxSequence = snd . foldl' step (0, 0)
+  where
+    step (lastSum, currMax) el = join bimap (max $ lastSum + el) (0, currMax)
+
+-- Other solutions
+
+maxSequence1 :: [Int] -> Int
+maxSequence1 = maximum . scanl ((+) . max 0) 0
